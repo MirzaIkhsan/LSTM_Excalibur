3535efdd6d49f8e49363ea30e8856813f8f2e9cb
hasnaroihan1 <hasnarn20@gmail.com>
2021-10-26 18:39:41 +0700

    init forget gate

---
+++ Forget.py
@@ -0,0 +1,14 @@
+import math
+import numpy as np
+
+class Forget():
+    def __init__(self, U, W, bias=1):
+        self.U = U
+        self.W = W
+        self.bias = bias
+
+    def _sigmoid(self, number):
+        return 1/(1+math.exp(-number))
+
+    def score(x, h_prev):
+        return self._sigmoid(np.matmul(self.U, x) + np.matmul(self.W, h_pref) + self.bias)
