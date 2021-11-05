65a6a6a2039791d337254cdf3f6ab82ed017907f
Difa Habiba Rahman <difa.habiba.rahman@gmail.com>
2021-11-05 08:57:37 +0700

    Docs InputGate

--- InputGate.py
+++ InputGate.py
@@ -5,6 +5,17 @@ from ActivationFunction import ActivationFunction
 
 class InputGate():
     def __init__(self, Ui, Wi, Uc, Wc, bi, bct):
+        '''
+        Konstruktor untuk Input Gate.
+
+        Parameters:
+            Ui: Matriks bobot untuk menghitung input ke-t dari nilai x ke-t.
+            Wi: Matriks bobot untuk menghitung input ke-t dari nilai h pada timestep sebelumnya.
+            Uc: Matriks bobot untuk menghitung candidate ke-t dari nilai x ke-t.
+            Wc: Matriks bobot untuk menghitung candidate ke-t dari nilai h pada timestep sebelumnya.
+            bi: bias untuk input ke-t
+            bct: bias untuk candidate
+        '''
         self.Ui = Ui
         self.Wi = Wi
         self.Uc = Uc
@@ -13,6 +24,16 @@ class InputGate():
         self.bct = bct
 
     def score_it(self, x, h_prev):
+        '''
+        Menghitung nilai i untuk timestep t.
+
+        Parameters:
+            x:      Nilai x (data masukan) untuk timestep t.
+            h_prev: Nilai hidden (h) dari timestep sebelumnya. 
+
+        Returns:
+            Numpy array berisi hasil dari perhitungan.
+        '''
         res = np.matmul(self.Ui, x.transpose()) + np.matmul(self.Wi, h_prev) + self.bi
         # if type(res) == np.float64:
         #     print(res)
@@ -21,6 +42,16 @@ class InputGate():
         return self.it
 
     def score_ct(self, x, h_prev):
+        '''
+        Menghitung nilai candidate untuk timestep t.
+
+        Parameters:
+            x:      Nilai x (data masukan) untuk timestep t.
+            h_prev: Nilai hidden (h) dari timestep sebelumnya. 
+
+        Returns:
+            Numpy array berisi hasil dari perhitungan.
+        '''
         self.ct = np.tanh(np.matmul(self.Uc, x.transpose()) + np.matmul(self.Wc, h_prev) + self.bct)
         return self.ct
 
